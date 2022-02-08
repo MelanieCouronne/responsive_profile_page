98962fa4cabc94d9058598284b2ef7c936984932
Melanie Couronne <melanie.couronne@gmail.com>
2022-02-08 14:54:44 +0100

    JS optimization on DOM

--- javascript/readMore.js
+++ javascript/readMore.js
@@ -1,26 +1,27 @@
 const readMore = () => {
   // Variables
-  const triggerOverflow = document.getElementById('content-overflow');
   const containerHeight = document.querySelector('.card-md');
-  const changedChevron = document.querySelector('.fa-chevron-down');
-  const btnTrigger = document.getElementById('read-more');
+  const triggerOverflow = containerHeight.querySelector('#content-overflow');
+  const behaviourBtn = containerHeight.querySelector('#btn-more');
+  const btnText = behaviourBtn.querySelector('#read-more');
+  const rotatedChevron = behaviourBtn.querySelector('.fa-chevron-down');
 
   // Handle the Read More effect
-  if (btnTrigger) {
-    if (btnTrigger.innerHTML === "Read More") {
+  if (behaviourBtn) {
+    if (btnText.innerHTML === "Read More") {
       containerHeight.classList.add('more-height');
       triggerOverflow.classList.remove('overflow');
       // Handle the text
-      btnTrigger.innerHTML = "Read Less";
+      btnText.innerHTML = "Read Less";
       // Handle the chevron turnover
-      changedChevron.classList.add('turnover-transition');
+      rotatedChevron.classList.add('turnover-transition');
     } else {
       containerHeight.classList.remove('more-height');
       triggerOverflow.classList.add('overflow');
       // Handle the text
-      btnTrigger.innerHTML = "Read More";
+      btnText.innerHTML = "Read More";
       // Handle the chevron turnover
-      changedChevron.classList.remove('turnover-transition');
+      rotatedChevron.classList.remove('turnover-transition');
     }
   }
 };
